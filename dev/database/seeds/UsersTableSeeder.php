36ffccec680c318e07e38a2581e2d4c8b32230e1
Francois <francois@assemble.co.za>
2018-04-18 08:23:42 +0000

    Added basic auth and seeder for user [MM-4]

---
+++ dev/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       /*
+        *Default admin user
+        */
+        $user_id =  DB::table('users')->insertGetId([
+                'name' => "admin",
+                'email' => "francois@assemble.co.za",
+                'password' => bcrypt('jM68b7LdRspcHhMv'),
+                'remember_token' => null,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+        ]);
+    }
+}
