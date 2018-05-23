11e44128d75f79519244b8a15b8eadbb14e7fcb8
Francois <francois@assemble.co.za>
2018-05-23 14:33:55 +0000

    Changed footer to design

--- dev/resources/views/layouts/elements/trade_footer.blade.php
+++ dev/resources/views/layouts/elements/trade_footer.blade.php
@@ -3,10 +3,11 @@
 		<div class="row">
 			<div class="col col-lg-4 mt-2 mb-2">
 				<a class="footer-title-image" href="{{ url('/') }}">
-          			<span class="icon icon-mm-logo"></span>
+          			<span class="icon icon-mm-logo-v2"></span>
+          			<span class="footer-logo-text">All rights reserved @ 2018</span>
         		</a>	
 			</div>
-			<div class="col col-lg-8 mt-2 mb-2 pt-3">
+			<div class="col col-lg-8 mt-2 mb-2 pt-2">
 				<div class="footer-links-block">
 					<a class="footer-link active mr-4" href="">Colours Explained</a>
 					<a class="footer-link active mr-4" href="">Fee Structure</a>
