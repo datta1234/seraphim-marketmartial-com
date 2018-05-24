7e8a4ce6ec126a269b41d38dd94185fef1edb7db
Alex Blake <alex@assemble.co.za>
2018-05-24 10:09:34 +0200

    starting interaction bar sub componenets

---
+++ dev/resources/assets/js/components/InteractionBar/MarketRequestSingleStock.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    const Market = reuqire('../../lib/Market');
+    export default {
+        props: {
+            market: {
+                type: Market
+            }
+        },
+        data() {
+            return {
+                opened: false,
+                subject: null
+            };
+        },
+        methods: {
+            toggleBar(set, subject) {
+                this.subject = subject;
+                if(typeof set != 'undefined') {
+                    this.opened = set == true;
+                } else {
+                    this.opened = !this.opened;
+                }
+            }
+        },
+        mounted() {
+            EventBus.$on('interactionToggle', this.toggleBar)
+        }
+    }
+</script>
