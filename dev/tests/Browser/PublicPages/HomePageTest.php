141d4c29d3546fc1e466b83532c2ca420b3fab8a
Francois <francois@assemble.co.za>
2018-06-01 12:37:44 +0000

    Addded role factory and amended the Homepage browser tests, also added id's to homepage forms

--- dev/tests/Browser/PublicPages/HomePageTest.php
+++ dev/tests/Browser/PublicPages/HomePageTest.php
@@ -5,11 +5,15 @@ namespace Tests\Browser\PublicPages;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-
+use App\Models\UserManagement\User;
+use App\Models\UserManagement\Role;
+use App\Models\UserManagement\Organisation;
 use Tests\Browser\Pages\HomePage;
+use Tests\Browser\Pages\TradeScreen;
 
 class HomePageTest extends DuskTestCase
 {
+    use DatabaseMigrations;
     /**
      * A Dusk test example.
      *
@@ -27,16 +31,19 @@ class HomePageTest extends DuskTestCase
 
     public function testLogin()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit(new HomePage);
-
-            // Login box
-            $this->assertTrue($browser->element('input[type="hidden"][name="_token"]'));
-            $this->assertTrue($browser->element('input[type="email"][name="email"]'));
-            $this->assertTrue($browser->element('input[type="password"][name="password"]'));
-            $this->assertTrue($browser->element('input[type="submit"]'));
-            
-            $browser->assertSeeLink('Forgot Password');
+        $this->browse(function ($browser) {
+            $role = factory(Role::class)->create();
+            $organisation = factory(Organisation::class)->create();
+            $user = factory(User::class)->create([
+                        'organisation_id' =>  $organisation->id,
+                        'password'  =>  \Hash::make('samplepass')
+                    ]);
+            $browser->visit(new HomePage)
+                    ->type('#homePageLoginForm input[name="email"]', $user->email)
+                    ->type('#homePageLoginForm input[name="password"]', 'samplepass')
+                    ->press('#homePageLoginForm button[type="submit"]')
+                    ->waitForLocation((new TradeScreen)->url())
+                    ->assertSeeLink('Logout');
         });
     }
 
@@ -44,7 +51,8 @@ class HomePageTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit(new HomePage)
-
+                ->assertSee('Active Market Makers Online')
+                
                 // see the main phrase
                 ->assertSee('The Inter-Bank Derivatives')
                 ->assertSee('Trading Platform')
@@ -61,6 +69,28 @@ class HomePageTest extends DuskTestCase
                 ->assertSeeLink('About Us')
                 ->assertSeeLink('Contact Us')
                 ->assertSeeLink('Sign up now');
+        });
+    }
+
+    public function testContact()
+    {
+        $this->browse(function ($browser) {
+            $browser->visit(new HomePage)
+                    ->type('#homeContactForm input[name="name"]', 'Test name')
+                    ->type('#homeContactForm input[name="email"]', 'test@sample.com')
+                    ->type('#homeContactForm textarea[name="message"]', 'This is a sample message')
+                    ->press('#homeContactForm button[type="submit"]')
+                    ->waitForLocation((new HomePage)->url())
+                    ->assertSee('Contact message has been sent');
+        });
+    }
+
+    public function testComponents() 
+    {
+        $this->browse(function ($browser) {
+            $browser->visit(new HomePage)
+                    ->assertVisible('#main-footer')
+                    ->assertVisible('#mainNav');
         });
     }
 
