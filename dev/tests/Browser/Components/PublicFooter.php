b769a5a6c1356999865085b6b9f4341eb5fc7db7
vagrant <vagrant@homestead>
2018-06-05 09:07:10 +0000

    Added Browser test componenets for nav public footer and trade footer

---
+++ dev/tests/Browser/Components/PublicFooter.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Browser\Components;
+
+use Laravel\Dusk\Browser;
+use Laravel\Dusk\Component as BaseComponent;
+
+class PublicFooter extends BaseComponent
+{
+    /**
+     * Get the root selector for the component.
+     *
+     * @return string
+     */
+    public function selector()
+    {
+        return '#main-footer';
+    }
+
+    /**
+     * Assert that the browser page contains the component.
+     *
+     * @param  Browser  $browser
+     * @return void
+     */
+    public function assert(Browser $browser)
+    {
+        $browser->assertVisible($this->selector());
+    }
+
+    /**
+     * Get the element shortcuts for the component.
+     *
+     * @return array
+     */
+    public function elements()
+    {
+        return [
+            '@footer-links' => $this->selector() . ' .footer-links-block',
+        ];
+    }
+
+    /**
+     * Assert that the footer links are present.
+     *
+     * @return void
+     */
+    public function testContent(Browser $browser)
+    {
+        $browser->assertSeeLink('T&Cs')
+                ->assertSeeLink('Privacy Policy')
+                ->assertSeeLink('Conflict of interest')
+                ->assertSeeLink('Compliance overview')
+                ->assertSeeLink('FSP specific documentation');
+    }
+}
---
+++ dev/tests/Browser/Components/TradeFooter.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Browser\Components;
+
+use Laravel\Dusk\Browser;
+use Laravel\Dusk\Component as BaseComponent;
+
+class TradeFooter extends BaseComponent
+{
+    /**
+     * Get the root selector for the component.
+     *
+     * @return string
+     */
+    public function selector()
+    {
+        return '#trade-footer';
+    }
+
+    /**
+     * Assert that the browser page contains the component.
+     *
+     * @param  Browser  $browser
+     * @return void
+     */
+    public function assert(Browser $browser)
+    {
+        $browser->assertVisible($this->selector());
+    }
+
+    /**
+     * Get the element shortcuts for the component.
+     *
+     * @return array
+     */
+    public function elements()
+    {
+        return [
+            '@footer-links' => $this->selector() . ' .footer-links-block',
+        ];
+    }
+
+    /**
+     * Assert that the footer links are present.
+     *
+     * @return void
+     */
+    public function testContent(Browser $browser)
+    {
+        $browser->assertSeeLink('Colours Explained')
+                ->assertSeeLink('Fee Structure')
+                ->assertSeeLink('Conditions Explained')
+                ->assertSee('All rights reserved @ 2018');
+    }
+}
