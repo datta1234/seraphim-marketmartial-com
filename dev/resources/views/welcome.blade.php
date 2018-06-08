d3c7014363ffc3bb6453d5e998fc080c0eb8ba2e
Francois <francois@assemble.co.za>
2018-06-08 06:54:35 +0000

    removed test code from contact form action

--- dev/resources/views/welcome.blade.php
+++ dev/resources/views/welcome.blade.php
@@ -311,7 +311,7 @@
         <div class="row">
             <div class="col col-12 col-md-6 col-lg-4 offset-lg-1 footer-sections pt-3 pb-3">
                 <h3>Send us a message</h3>
-                <form id="ContactUsForm" action="{{ route('contact') }}#anchor" method="POST">
+                <form id="ContactUsForm" action="{{ route('contact') }}" method="POST">
                      {{ csrf_field() }}
                     <div class="form-group mb-2">
                         <input type="text" class="form-control {{ $errors->has('name') ? ' is-invalid' : '' }}" value="{{ old('name') }}" id="name" name="name" placeholder="Enter your name here...">
