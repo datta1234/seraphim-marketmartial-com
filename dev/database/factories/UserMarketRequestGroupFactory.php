6d481383093e9bc94e18d7c4ebc1aefdd87adee2
Sizwe <sizwe@assemble.co.za>
2018-06-14 13:42:33 +0200

    fix usermaketrequest and create factory for it

---
+++ dev/database/factories/UserMarketRequestGroupFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\MarketRequest\UserMarketRequestGroup::class, function (Faker $faker) {
+    return [
+		"user_market_request_id" => factory(App\Models\MarketRequest\UserMarketRequest::class)->create()->id,
+		"trade_structure_group_id" => factory(App\Models\StructureItems\TradeStructure::class,'Outright')->create()->id,
+		"is_selected" => rand(0,1) ==1
+    ];
+});
+
