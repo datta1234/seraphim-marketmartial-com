c8ea7b6b0332aacb7fb81863663289442ccd17da
Shaun Hogan <shaun@assemble.co.za>
2018-06-18 11:51:17 +0200

    Fixed Broken factorys

--- dev/database/factories/TradeFactory.php
+++ dev/database/factories/TradeFactory.php
@@ -4,14 +4,13 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Trade\Trade::class, function (Faker $faker) {
     return [
-        'contracts'				=> randomFloat(2, 1, 99999999999 ),
-        'trade_negotiation_id'	=> factory( App\Models\Trade\TradeNegotiation::class )->creat()->id,
-        'market_negotiation_id'	=> factory( App\Models\Market\MarketNegotiation::class )->craete()->id,
-        'user_market_id'		=> factory( App\Models\Market\UserMarket::class )->craete()->id,
+        'trade_negotiation_id'	=> factory( App\Models\Trade\TradeNegotiation::class )->create()->id,
+        'market_negotiation_id'	=> factory( App\Models\Market\MarketNegotiation::class )->create()->id,
+        'user_market_id'		=> factory( App\Models\Market\UserMarket::class )->create()->id,
         'initiate_user_id'		=> factory( App\Models\UserManagement\User::class )->create()->id,
         'recieving_user_id'		=> factory( App\Models\UserManagement\User::class )->create()->id,
         'trade_status_id'		=> factory( App\Models\Trade\TradeStatus::class )->create()->id,
-        'quantity'				=> randomFloat(2, 1, 99999999999 )
+        'quantity'				=> $faker->randomFloat(2, 0, 9999 )
 
     ];
 });
