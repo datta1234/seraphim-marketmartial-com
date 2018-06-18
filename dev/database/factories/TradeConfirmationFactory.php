e1239c58f63e1b28884f72327ba20bf2b314f94f
Shaun Hogan <shaun@assemble.co.za>
2018-06-18 09:50:32 +0200

    TradeConfirmationFactory Created

---
+++ dev/database/factories/TradeConfirmationFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\TradeConfirmations\TradeConfirmation::class, function (Faker $faker) {
+    return [
+        'send_user_id'					=> factory(App\Models\UserManagement\User::class)->create()->id,
+        'receiving_user_id'				=> factory(App\Models\UserManagement\User::class)->create()->id,
+        'trade_id'						=> factory(App\Models\Trade\Trade::class)->create()->id,
+		'spot_price'					=> ( rand(0,10) > 4 ) ? randomFloat(2, 1, 99999999999 ) : NULL,
+		'future_reference'				=> ( rand(0,10) > 4 ) ? randomFloat(2, 1, 99999999999 ) : NULL,
+		'near_expiery_reference'		=> ( rand(0,10) > 4 ) ? randomFloat(2, 1, 99999999999 ) : NULL,
+		'trade_confirmation_id'			=> NULL,
+		'trade_confirmation_status_id'	=> factory( App\Models\Trade\TradeStatus::class )->create()->id,
+		'market_id'						=> NULL,
+		'stock_id'						=> NULL,
+		'contracts'						=> randomFloat(2, 1, 99999999999 ),
+		'puts'							=> randomFloat(2, 1, 99999999999 ),
+		'calls'							=> randomFloat(2, 1, 99999999999 ),
+		'delta'							=> randomFloat(2, 1, 99999999999 ),
+		'gross_premiums'				=> randomFloat(2, 1, 99999999999 ),
+		'net_premiums'					=> randomFloat(2, 1, 99999999999 ),
+		'is_confirmed'					=> $faker->boolean(50),
+		'traiding_account_id'			=> factory(App\Models\UserManagement\TradingAccount::class)->create()->id
+    ];
+});
