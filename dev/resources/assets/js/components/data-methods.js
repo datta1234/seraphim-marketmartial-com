ed9ea23c73b7517651a6d27732f3f309be117105
Sizwe Williams <sizwe@assemble.co.za>
2018-07-09 15:27:40 +0200

    Update data-methods.js

--- dev/resources/assets/js/components/data-methods.js
+++ dev/resources/assets/js/components/data-methods.js
@@ -55,17 +55,17 @@ $( document ).ready(function() {
         };
         //Listens for input change on name input
         $( '#ContactUsForm' ).find('input#name').on("input", function() {
-            console.log("name Type");
+        //    console.log("name Type");
             checkContactForm($('#ContactUsForm'));
         });
         //Listens for input change on email input
         $( '#ContactUsForm' ).find('input#contactEmail').on("input", function() {
-            console.log("contact Type");
+        //    console.log("contact Type");
             checkContactForm($('#ContactUsForm'));
         });
         //Listens for input change on message textarea
         $( '#ContactUsForm' ).find('textarea#contact_message').on("input", function() {
-            console.log("message Type");
+        //    console.log("message Type");
             checkContactForm($('#ContactUsForm'));
         });
 
