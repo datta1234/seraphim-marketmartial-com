9bc71d305beb026b724415592c05f970ebd84e09
Sizwe <sizwe@assemble.co.za>
2018-09-25 09:40:24 +0200

    fix organisation uuid

--- dev/app/Jobs/UuidGen.php
+++ dev/app/Jobs/UuidGen.php
@@ -7,9 +7,8 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use App\Models\UserManagement\User;
-use App\Models\UserManagement\Organisation;
 use Illuminate\Support\Facades\Cache;
+use App\Helpers\Misc\ResolveUuid;
 
 
 class UuidGen implements ShouldQueue
@@ -33,63 +32,8 @@ class UuidGen implements ShouldQueue
      */
     public function handle()
     {
-        $expiresAt = now()->addHours(24);
-        Cache::put('usersMap',$this->generateUsersUuid(),$expiresAt);
-        Cache::put('organisationsMap',$this->generateOrganisationUuid(),$expiresAt);
+        ResolveUuid::generateOrganisationUuid();
+        ResolveUuid::generateUsersUuid();
     }
-
-    public function generateUsersUuid()
-    {
-        $users = User::all();
-        $userUuid = [];
-
-        foreach ($users as $user) 
-        {
-            $key = $this->gen_uuid();
-            while(!in_array($key, $userUuid))
-            {
-                $userUuid[$user->id] = $key; 
-            }
-        }
-        return $userUuid;
-    }
-
-    public function generateOrganisationUuid()
-    {
-        $organisations = Organisation::all();
-        $orgUuid = [];
-
-        foreach ($organisations as $organisation) 
-        {
-            $key = $this->gen_uuid();
-            while(!in_array($key, $orgUuid))
-            {
-                 $orgUuid[$organisation->id] = $key; 
-            }
-        }
-        return $orgUuid;
-    }
-
-    private function gen_uuid() 
-    {
-        return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for "time_low"
-            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
-
-            // 16 bits for "time_mid"
-            mt_rand( 0, 0xffff ),
-
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
-            mt_rand( 0, 0x0fff ) | 0x4000,
-
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand( 0, 0x3fff ) | 0x8000,
-
-            // 48 bits for "node"
-            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
-    );
-}
+  
 }
