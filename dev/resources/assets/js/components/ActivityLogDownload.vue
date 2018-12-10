233aea9b85c42d5c9cabb85b546d497420eb9c0e
Alex Blake <alex@assemble.co.za>
2018-12-10 17:07:25 +0200

    minor layout fix

--- dev/resources/assets/js/components/ActivityLogDownload.vue
+++ dev/resources/assets/js/components/ActivityLogDownload.vue
@@ -1,54 +1,48 @@
 <template>
     <form :action="action" :method="method">
         <!-- <div class="form-group row">
-            <label for="email" class="col-sm-2 col-form-label">Email address</label>
-            <div class="col-sm-10">
+            <label for="email" class="col-2 col-form-label">Email address</label>
+            <div class="col-10">
                 <input type="email" class="form-control" id="email" placeholder="Enter email">
             </div>
         </div> -->
         <div class="form-group row">
-            <label for="organisation" class="col-sm-2 col-form-label">Organisation</label>
-            <div class="col-sm-10">
+            <label for="organisation" class="col-2 col-form-label">Organisation</label>
+            <div class="col-10">
                 <b-form-select name="organisation" v-model="org_id" :options="org_list" @change="loadUsers" :disabled="loading" placeholder="Select One..."/>
             </div>
         </div>
         <div class="form-group row">
-            <label for="username" class="col-sm-2 col-form-label">Username</label>
-            <div class="col-sm-10">
+            <label for="username" class="col-2 col-form-label">Username</label>
+            <div class="col-10">
                 <b-form-select name="user" v-model="user_id" :options="user_list" :disabled="loading || org_id == ''" placeholder="Select One..."/>
             </div>
         </div>
         <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Date range</label>
-            <div class="col-sm-10">
+            <label class="col-2 col-form-label">Date range</label>
+            <div class="col-10">
                 <div class="row">
-                    <div class="col-sm-5">
-                        <div class="row">
-                            <label for="start_date" class="col-sm-2 col-form-label">From</label>
-                            <div class="col-sm-10">
-                                <input name="start_date" type="date" class="form-control" id="start_date" :disabled="loading" placeholder="">
-                            </div>
-                        </div>
+                    <label for="start_date" class="col-2 col-form-label">From</label>
+                    <div class="col-4">
+                        <input name="start_date" type="date" class="form-control" id="start_date" :disabled="loading" placeholder="">
                     </div>
-                    <div class="col-sm-5">
-                        <div class="row">
-                            <label for="end_date" class="col-sm-2 col-form-label">To</label>
-                            <div class="col-sm-10">
-                                <input name="end_date" type="date" class="form-control" id="end_date" :disabled="loading" placeholder="">
-                            </div>
-                        </div>
+                    <label for="end_date" class="col-2 col-form-label">To</label>
+                    <div class="col-4">
+                        <input name="end_date" type="date" class="form-control" id="end_date" :disabled="loading" placeholder="">
                     </div>
                 </div>
             </div>
         </div>
         <div class="form-group row">
-            <label for="activity_type" class="col-sm-2 col-form-label">Activity Type</label>
-            <div class="col-sm-10">
+            <label for="activity_type" class="col-2 col-form-label">Activity Type</label>
+            <div class="col-10">
                 <b-form-select name="activity_type" v-model="activity_type" :options="activity_type_list" :disabled="loading" placeholder="Select One..."/>
             </div>
         </div>
         <div class="form-group row">
-            <button type="submit" class="btn mm-generic-trade-button w-100" :disabled="loading">Download Logs</button>
+            <div class="col-10 offset-1">
+                <button type="submit" class="btn mm-generic-trade-button w-100" :disabled="loading">Download Logs</button>
+            </div>
         </div>
     </form>
 </template>
--- dev/resources/views/admin/logging/index.blade.php
+++ dev/resources/views/admin/logging/index.blade.php
@@ -2,18 +2,22 @@
 
 @section('content')
     <div class="container-fluid" id="activity_log_app">
-        {{-- Logging Card --}}
-        @component('partials.content_card')
-            @slot('header')
-                <h2 class="mt-2 mb-2">Activity Logs</h2>
-            @endslot
-            @slot('title')
-            @endslot
-            @slot('decorator')
-            @endslot
-            @slot('body')
-                <activity-log-modal :activity-types="activity_types" action="{{ route('admin.logging.download') }}" method="GET"></activity-log-modal>
-            @endslot
-        @endcomponent
+        <div class="row">
+            <div class="col-6 offset-3">
+                {{-- Logging Card --}}
+                @component('partials.content_card')
+                    @slot('header')
+                        <h2 class="mt-2 mb-2">Activity Logs</h2>
+                    @endslot
+                    @slot('title')
+                    @endslot
+                    @slot('decorator')
+                    @endslot
+                    @slot('body')
+                        <activity-log-download :activity-types="activity_types" action="{{ route('admin.logging.download') }}" method="GET"></activity-log-download>
+                    @endslot
+                @endcomponent
+            </div>
+        </div>
     </div>
 @endsection
