149939391e2ab42870d0b46108cd56beadd71858
Alex Blake <alex@assemble.co.za>
2019-01-04 15:19:19 +0200

    added quotes confirm message [MM-752]

--- dev/resources/assets/js/components/MarketGroupComponent.vue
+++ dev/resources/assets/js/components/MarketGroupComponent.vue
@@ -25,9 +25,6 @@
                         <market-tab :market-request="m_req" :key="m_req.id" v-for="(m_req,m_req_index) in market_date_groups[date]" :no_cares="no_cares"></market-tab>
                     </div><!-- END Date collection section -->
                 </div>
-
-                <div>
-                </div>
             </div>
         </div>
     </div>
