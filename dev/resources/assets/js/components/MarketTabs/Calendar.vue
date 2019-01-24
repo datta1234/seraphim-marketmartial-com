ee789edae2dcda234f562317b79e945683d84c8a
Alex Blake <alex@assemble.co.za>
2019-01-24 09:58:10 +0200

    updated layout of the market tabs [MM-887]

--- dev/resources/assets/js/components/MarketTabs/Calendar.vue
+++ dev/resources/assets/js/components/MarketTabs/Calendar.vue
@@ -16,28 +16,22 @@
                 </b-row>
             </b-col>
             <b-col class="market-tab-state">
-                <b-row align-v="center" align-h="center" class="h-100">
-                    <b-col v-if="market_request_state_label != ''">
+                <b-row align-v="center" align-h="center" class="h-100" v-if="market_request_state_label != ''">
+                    <b-col>
                         <span v-bind:class="{'user-action': market_request_state_label == 'SENT'}" class="">{{ market_request_state_label }}</span>
                     </b-col>
-                    <b-col v-else>
-                        <b-row>
-                            <b-col>
-                                <span v-if="choice_vol_1 != null">{{ choice_vol_1 }}</span>
-                                <span v-else>&nbsp;</span>
-                            </b-col>
-                        </b-row>
-                        <b-row>
-                            <b-col>
-                                <span class="" v-bind:class="bidState">{{ user_market_bid }}</span>&nbsp;/&nbsp;<span class="" v-bind:class="offerState">{{ user_market_offer }}</span>
-                            </b-col>
-                        </b-row>
-                        <b-row>
-                            <b-col>
-                                <span v-if="choice_vol_2 != null">{{ choice_vol_2 }}</span>
-                                <span v-else>&nbsp;</span>
-                            </b-col>
-                        </b-row>
+                </b-row>
+                <b-row align-v="center" align-h="center" class="h-100" v-else>
+                    <b-col cols="12" v-if="choice_vol_1 != null">
+                        <span>{{ choice_vol_1 }}</span>
+                    </b-col>
+                    <b-col cols="12">
+                        <span class="" v-bind:class="bidState">{{ user_market_bid }}</span>&nbsp;/&nbsp;<span class="" v-bind:class="offerState">
+                            {{ user_market_offer }}
+                        </span>
+                    </b-col>
+                    <b-col cols="12" v-if="choice_vol_2 != null">
+                        <span>{{ choice_vol_2 }}</span>
                     </b-col>
                 </b-row>
             </b-col>
