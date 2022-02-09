bd477ce5a311c0ef78d26c27886140c685430622
Francois <francois@assemble.co.za>
2022-02-09 15:54:23 +0200

    Added custom favicons

--- dev/resources/views/stats/open_interest.blade.php
+++ dev/resources/views/stats/open_interest.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.canvas_app')
 
+@section('favicon')
+	<link rel="shortcut icon" href="{{ asset('favicon_alt.ico') }}">
+@endsection
+
 @section('content')
 	<div class="container">
 		@include('partials.stats_navigation')
